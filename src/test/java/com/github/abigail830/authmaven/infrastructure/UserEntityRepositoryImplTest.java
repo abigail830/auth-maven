22a9035d0993d2faf0e1c10dbb3731f8138e1986
SaraQian <abigail830@163.com>
2019-11-11 19:50:57 +0800

    fix test case

--- src/test/java/com/github/abigail830/authmaven/infrastructure/UserEntityRepositoryImplTest.java
+++ src/test/java/com/github/abigail830/authmaven/infrastructure/UserEntityRepositoryImplTest.java
@@ -2,33 +2,36 @@ package com.github.abigail830.authmaven.infrastructure;
 
 import com.github.abigail830.authmaven.infrastructure.entity.RoleEntity;
 import com.github.abigail830.authmaven.infrastructure.entity.UserEntity;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@DBRider
 class UserEntityRepositoryImplTest {
 
     @Autowired
     UserRepositoryImpl userRepository;
 
     @Test
+    @DataSet(value = "userEntityRepoTest.yml", cleanBefore = true)
     void getUserByName() {
-
-        final UserEntity userEntity = userRepository.getUserByName("userEntity");
-        assertEquals("e10adc3949ba59abbe56e057f20f883e", userEntity.getPassword());
+        final UserEntity userEntity = userRepository.getUserByName("user");
+        assertEquals("password1", userEntity.getPassword());
         assertEquals(1L, userEntity.getId());
     }
 
     @Test
+    @DataSet(value = "userEntityRepoTest.yml", cleanBefore = true)
     void getRoleByUserId() {
-
         final List<RoleEntity> roleEntityByUserId = userRepository.getRoleByUserId(1L);
         assertEquals(1, roleEntityByUserId.size());
-        assertEquals("USER", roleEntityByUserId.get(0).getName());
+        assertEquals("ROLE_USER", roleEntityByUserId.get(0).getName());
     }
 }
